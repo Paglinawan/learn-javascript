bdb1ca8aefe44d2e0425544052e6de79197809c7
miho <paglinawan0520@gmail.com>
2023-12-08 17:59:39 +0900

    Learn: await and async

---
+++ js/async-await.js
@@ -0,0 +1,19 @@
+function sleep(val) {
+  return new Promise(function (resolve) {
+    setTimeout(function () {
+      console.log(val++);
+      resolve(val);
+    }, 1000);
+  });
+}
+
+async function init() {
+  let val = await sleep(0);
+  val = await sleep(val);
+  val = await sleep(val);
+  val = await sleep(val);
+  val = await sleep(val);
+  console.log(val);
+}
+
+init();
