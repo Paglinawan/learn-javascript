314fb43ed856bf9e2566e98aca1e9f53b136f9d8
miho <paglinawan0520@gmail.com>
2023-12-08 18:12:25 +0900

    Update: then -> await

--- js/fetch.js
+++ js/fetch.js
@@ -1,11 +1,8 @@
-fetch("https://jsonplaceholder.typicode.com/users")
-  .then(function (res) {
-    console.log(res);
-    return res.json();
-  })
-  .then(function (json) {
-    console.log(json);
-    for (const user of json) {
-      console.log(`I'm ${user.name}`);
-    }
-  });
+async function fetchUsers() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const json = await res.json();
+  for (const user of json) {
+    console.log(`I'm ${user.name}`);
+  }
+}
+fetchUsers();
