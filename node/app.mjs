20c8cbb99d47ce8f9359ed6a10cc6edec4175ad0
miho <paglinawan0520@gmail.com>
2023-12-14 20:30:00 +0900

    Learn: content-type

--- node/app.mjs
+++ node/app.mjs
@@ -2,7 +2,20 @@ import * as http from "http";
 
 const server = http.createServer(function (req, res) {
   console.log(req.url);
-  res.end("Hello World!");
+  res.writeHead(200, { "content-type": "text/html; charset=utf-8" });
+  res.end(`
+  <!DOCTYPE html>
+  <html lang="ja">
+    <head>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Home</title>
+    </head>
+    <body>
+      <h1>こんにちは！</h1>
+    </body>
+  </html>
+  `);
 });
 
 server.listen(8080);
