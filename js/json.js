ec8eb99e0419fdcf0cfe4dfa6a48fcb2ff3101e6
miho <paglinawan0520@gmail.com>
2023-12-18 21:55:26 +0900

    Adjust: JSON (array)

--- js/json.js
+++ js/json.js
@@ -1,9 +1,4 @@
 const obj = { a: 0, b: 1, c: 2 };
 
-function replacer(prop, value) {
-  if (value < 1) return;
-  return value;
-}
-
-const json = JSON.stringify(obj, replacer);
+const json = JSON.stringify(obj, ["a", "b"]);
 console.log(json);
