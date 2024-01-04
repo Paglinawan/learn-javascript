1ba627c3826353591079f0e5ca8c0a2ec9104ac2
miho <paglinawan0520@gmail.com>
2024-01-04 22:06:13 +0900

    Learn: spread operator

--- js/array.js
+++ js/array.js
@@ -1,4 +1,3 @@
 const array = [1, 2, 3, 4, 5];
-const result = array.splice(1, 1);
-console.log(array);
-console.log(`戻り値: ${result}`);
+const newArray = [0, ...array, 6, 7, 8];
+console.log(newArray);
