06fb3ddd1b484bee4dc4b6a8fabcfbaf3d9deab7
miho <paglinawan0520@gmail.com>
2024-01-05 21:19:09 +0900

    Learn: function and copy

--- js/copy.js
+++ js/copy.js
@@ -1,10 +1,13 @@
-// const a = "Hello";
-// a = "Bye";
-// console.log(a); // -> Error
+let a = 0;
 
-const b = {
-  prop: "Hello",
-};
+function fn1(arg1) {
+  arg1 = 1;
+  console.log(a, arg1);
+}
+fn1(a);
 
-b.prop = "Bye";
-console.log(b);
+let arg1 = a;
+arg1 = 1;
+console.log(a, arg1);
+
+// -> プリミティブ型を関数の引数として渡す場合には値はそれぞれ独立しているので互いに影響を受けない
