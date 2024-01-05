d7627ab9ce2e2bd21364e5eddab6710f3c7b9f67
miho <paglinawan0520@gmail.com>
2024-01-05 21:23:36 +0900

    Learn: function and copy object

--- js/copy.js
+++ js/copy.js
@@ -1,13 +1,17 @@
-let a = 0;
+let a = {
+  prop: 0,
+};
 
 function fn1(arg1) {
-  arg1 = 1;
+  arg1.prop = 1;
   console.log(a, arg1);
 }
 fn1(a);
 
-let arg1 = a;
-arg1 = 1;
-console.log(a, arg1);
+// -> オブジェクトを関数の引数として渡す場合にはオブジェクトへの参照がコピーされている
 
-// -> プリミティブ型を関数の引数として渡す場合には値はそれぞれ独立しているので互いに影響を受けない
+function fn2(arg2) {
+  arg2 = {};
+  console.log(a, arg2);
+}
+fn2(a);
