ec53e46753845b44939191bf549684e2e116783c
miho <paglinawan0520@gmail.com>
2024-01-05 20:59:43 +0900

    Learn: filter

--- js/array.js
+++ js/array.js
@@ -1,5 +1,5 @@
 const array = [1, 2, 3, 4, 5];
-const newArray = array.map(function (v, i, arry) {
-  return v * 10;
+const newArray = array.filter(function (v, i) {
+  return i >= 1;
 });
 console.log(array, newArray);
