a0f9acc73c6df0c38edfddd46fc9e91f6db116a5
miho <paglinawan0520@gmail.com>
2024-01-08 13:05:16 +0900

    Learn: reduce

--- js/array.js
+++ js/array.js
@@ -1,5 +1,6 @@
 const array = [1, 2, 3, 4, 5];
-const newArray = array.filter(function (v, i) {
-  return i >= 1;
-});
-console.log(array, newArray);
+const newArray = array.reduce(function (accu, curr) {
+  console.log(accu, curr);
+  return accu + curr;
+}, 0);
+console.log(newArray);
