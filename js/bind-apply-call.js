c5e8f63787ab8201433ae31b9f13f3437eaaf1a6
miho <paglinawan0520@gmail.com>
2024-01-08 12:41:57 +0900

    Learn: call

--- js/bind-apply-call.js
+++ js/bind-apply-call.js
@@ -1,9 +1,9 @@
-function a() {
-  console.log("Hello " + this.name);
+function a(message = "") {
+  console.log("Hello " + this.name + message);
 }
 
 const miho = { name: "Miho" };
 
 a.bind(miho); // 実行されない
 a.apply(miho);
-a.call(miho);
+a.call(miho, " How are you?"); // 引数を設定できる
