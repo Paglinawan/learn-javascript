29d33142eff137d983cf9fc212c5c9eee2707935
miho <paglinawan0520@gmail.com>
2024-01-10 17:46:16 +0900

    Learn: behavior of not object

--- js/constructor-return.js
+++ js/constructor-return.js
@@ -8,9 +8,8 @@ Object.prototype.c = function () {};
 
 function newFn(C, ...args) {
   const _this = Object.create(Constructor.prototype);
-  const result = C.apply(_this, args); // apply( {} , [ 1, 2 ] )
-  console.log(_this);
-  // -> 関数の引数としてオブジェクトを渡した場合,そのオブジェクトの参照を渡すので,実引数側のオブジェクトに影響が出る
+  const result = C.apply(_this, args);
+  return _this;
 }
 
 const instance = newFn(Constructor, 1, 2);
