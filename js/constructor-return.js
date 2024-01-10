6abc2b88f172e78990ffef7ddf99768e4ee16b5e
miho <paglinawan0520@gmail.com>
2024-01-10 17:15:23 +0900

    Learn: constructor and prototype

--- js/constructor-return.js
+++ js/constructor-return.js
@@ -9,6 +9,9 @@ function NotObject(a, b) {
   // return 10;
 }
 
+Object.prototype.c = function () {};
+NotObject.prototype.c = function () {};
+
 const instance1 = new Object(1, 2);
 const instance2 = new NotObject(1, 2);
 console.log(instance1);
