5e4c40d12c20295f5e5b2aee234ceec844db478e
miho <paglinawan0520@gmail.com>
2024-01-10 17:48:58 +0900

    Learn: behavior of object

--- js/constructor-return.js
+++ js/constructor-return.js
@@ -1,7 +1,7 @@
 function Constructor(a, b) {
   this.a = a;
   this.b = b;
-  // return {};
+  return {};
 }
 
 Object.prototype.c = function () {};
@@ -9,6 +9,9 @@ Object.prototype.c = function () {};
 function newFn(C, ...args) {
   const _this = Object.create(Constructor.prototype);
   const result = C.apply(_this, args);
+  if (typeof result === "object" && typeof result !== null) {
+    return result;
+  }
   return _this;
 }
 
