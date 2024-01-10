149ad9c742ae1430e1224dda5730adaef66dbf7f
miho <paglinawan0520@gmail.com>
2024-01-10 17:42:59 +0900

    Learn: object as arg of function

--- js/constructor-return.js
+++ js/constructor-return.js
@@ -7,8 +7,10 @@ function Constructor(a, b) {
 Object.prototype.c = function () {};
 
 function newFn(C, ...args) {
-  const _this = Object.create(Constructor.prototype); // __proto__に格納される空のオブジェクト
+  const _this = Object.create(Constructor.prototype);
+  const result = C.apply(_this, args); // apply( {} , [ 1, 2 ] )
   console.log(_this);
+  // -> 関数の引数としてオブジェクトを渡した場合,そのオブジェクトの参照を渡すので,実引数側のオブジェクトに影響が出る
 }
 
 const instance = newFn(Constructor, 1, 2);
