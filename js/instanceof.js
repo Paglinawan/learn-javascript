6a20d0d9418b5fd3ce398eafe7c336024fb25efa
miho <paglinawan0520@gmail.com>
2024-01-11 20:00:31 +0900

    Learn: instanceof when return object

--- js/instanceof.js
+++ js/instanceof.js
@@ -1,7 +1,8 @@
 function Constructor(a, b) {
   this.a = a;
   this.b = b;
+  return { a: 1 };
 }
 
 const instance = new Constructor(1, 2);
-console.log(instance instanceof Constructor);
+console.log(instance instanceof Constructor); // false
