0e415200d2cb61ef72097db61b9eb70fb1cf0e06
miho <paglinawan0520@gmail.com>
2024-01-11 20:17:44 +0900

    Learn: Function Constructor and global scope

--- js/function-constructor.js
+++ js/function-constructor.js
@@ -1,2 +1,9 @@
-const result = new Function("a", "b", "return a + b");
-console.log(result(1, 2));
+let c = 0;
+function fn() {
+  let c = 1;
+  const result = new Function("a", "b", "return a * b * c");
+  return result;
+}
+
+const fnChild = fn();
+console.log(fnChild(1, 2));
