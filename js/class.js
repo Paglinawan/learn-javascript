ed8e505d5a212bf3a75259d4f1484324a5e2410d
miho <paglinawan0520@gmail.com>
2024-01-13 18:26:23 +0900

    Learn: instance by Class

--- js/class.js
+++ js/class.js
@@ -7,12 +7,5 @@ class Person {
     console.log("Hello " + this.name);
   }
 }
-// ↑ Syntax Sugar
-// function Person(name,age) {
-//   this.name = name;
-//   this.age = age;
-// }
-
-// Person.prototype.hello = function () {
-//   console.log("Hello " + this.name);
-// };
+const Miho = new Person("Miho", 29);
+Miho.hello();
