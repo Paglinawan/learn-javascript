e8bcc09b6dec9890631f1877f0bd8c7f1a6fa05e
miho <paglinawan0520@gmail.com>
2024-01-17 21:03:27 +0900

    Learn: defineProperty

--- js/descriptor.js
+++ js/descriptor.js
@@ -1,4 +1,7 @@
-const obj = { prop: 0 };
+const obj = {};
+Object.defineProperty(obj, "prop", {
+  value: 0,
+});
 
 const descriptor = Object.getOwnPropertyDescriptor(obj, "prop");
 console.log(descriptor);
