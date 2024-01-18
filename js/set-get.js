4c593e47ba4d76408860af224ff3fe9b96cb6684
miho <paglinawan0520@gmail.com>
2024-01-18 21:14:47 +0900

    Update: getter / setter

--- js/set-get.js
+++ js/set-get.js
@@ -1,16 +1,15 @@
-function Person(name, age) {
-  this._name = name;
-  this._age = age;
-}
-
-Object.defineProperty(Person.prototype, "name", {
-  get: function () {
+class Person {
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
+  get name() {
     return `Hello ${this._name}`;
-  },
-  set: function (val) {
+  }
+  set name(val) {
     this._name = val;
-  },
-});
+  }
+}
 
 const miho = new Person("Miho", 29);
 console.log(miho.name);
