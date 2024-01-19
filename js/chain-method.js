b3016c3de9e3992d5485707a3b73bc4bbe99a828
miho <paglinawan0520@gmail.com>
2024-01-19 18:22:01 +0900

    Learn: class / constructor as arg

---
+++ js/chain-method.js
@@ -0,0 +1,20 @@
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  hello(person) {
+    //　 ↑ constructor(judy.name, judy.age)
+    console.log(`${this.name} says hello ${person.name}`);
+  }
+  shakeHands(person) {
+    console.log(`${this.name} shake hands with ${person.name}`);
+  }
+  bye(person) {
+    console.log(`Good bye ${person.name}.`);
+  }
+}
+
+const miho = new Person("Miho", 29);
+const judy = new Person("Judy", 36);
+miho.hello(judy);
