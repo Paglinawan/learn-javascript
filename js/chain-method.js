50e4f55b68a5ea89b9201eca30eac1f7c114494c
miho <paglinawan0520@gmail.com>
2024-01-19 18:28:24 +0900

    Learn: chain method

--- js/chain-method.js
+++ js/chain-method.js
@@ -6,17 +6,21 @@ class Person {
   hello(person) {
     //　 ↑ constructor(judy.name, judy.age)
     console.log(`${this.name} says hello ${person.name}`);
+
+    return this;
   }
   shakeHands(person) {
     console.log(`${this.name} shake hands with ${person.name}`);
+
+    return this;
   }
   bye(person) {
     console.log(`Good bye ${person.name}.`);
+
+    return this;
   }
 }
 
 const miho = new Person("Miho", 29);
 const judy = new Person("Judy", 36);
-miho.hello(judy);
-miho.shakeHands(judy);
-miho.bye(judy);
+miho.hello(judy).shakeHands(judy).bye(judy);
