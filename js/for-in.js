862a1e08fc11600a3902771be299aa6476638e9b
miho <paglinawan0520@gmail.com>
2024-01-21 21:50:17 +0900

    Update: symbol and for-in

--- js/for-in.js
+++ js/for-in.js
@@ -1,13 +1,11 @@
+const s = Symbol();
 const obj = {
   prop1: "value1",
   prop2: "value2",
   prop3: "value3",
+  [s]: "value4",
 };
 
-Object.prototype.method = function () {};
-
 for (let key in obj) {
-  if (obj.hasOwnProperty(key)) {
-    console.log(key, obj[key]);
-  }
+  console.log(key, obj[key]);
 }
