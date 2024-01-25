194ebb2111a2d0b02d265981a094aa07a4b41749
miho <paglinawan0520@gmail.com>
2024-01-25 18:19:51 +0900

    Update: for ... of

--- js/for-of.js
+++ js/for-of.js
@@ -1,5 +1,11 @@
 const array = ["a", "b", "c"];
 
+array[4] = "e"; // d undefined
+Object.prototype.method = function () {};
+Object.defineProperty(array, 0, {
+  enumerable: false,
+});
+
 for (let value of array) {
   console.log(value);
 }
