917dbdcac751d2b78c46974a92715232d69fa776
miho <paglinawan0520@gmail.com>
2024-01-28 18:13:52 +0900

    Learn: how to back array from set object

--- js/set.js
+++ js/set.js
@@ -5,6 +5,9 @@ const method = function () {};
 set.add(key);
 set.add(method);
 
-for (let key of set) {
-  console.log(key);
-}
+// for (let key of set) {
+//   console.log(key);
+// }
+
+const array = Array.from(set);
+console.log(array);
