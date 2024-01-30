0a0325398433b83b8d8f03fafff24a416999066d
miho <paglinawan0520@gmail.com>
2024-01-30 21:41:18 +0900

    Learn: iterator and object

--- js/iterator-2.js
+++ js/iterator-2.js
@@ -1,11 +1,27 @@
-const obj = {
+const items = {
   prop1: "value1",
   prop2: "value2",
   prop3: "value3",
 };
 
-const items = Object.entries(obj);
+Object.prototype[Symbol.iterator] = function () {
+  const keys = Object.keys(this);
+  let i = 0;
+  let _this = this; // オブジェクトのメソッドの場合thisの参照先は呼び出し元のオブジェクトになる
+  return {
+    next() {
+      let key = keys[i];
+      i++;
+      return {
+        value: [key, _this[key]],
+        done: i > keys.length,
+      };
+    },
+  };
+};
 
-for (const item of items) {
-  console.log(item);
+// const items = Object.entries(obj);
+
+for (const [k, v] of items) {
+  console.log(k, v);
 }
