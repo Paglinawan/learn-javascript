60ed425ae5123edea47f4eaa445b021f4f2840d3
miho <paglinawan0520@gmail.com>
2024-02-02 21:26:02 +0900

    Learn: Rest Parameter

--- js/spread-operator.js
+++ js/spread-operator.js
@@ -1,4 +1,15 @@
-const array = [1, 2, 3, 4, 5];
-const newArray = [...array];
-console.log(newArray);
-console.log(array === newArray);
+// const array = [1, 2, 3, 4, 5];
+// const newArray = [...array];
+// console.log(newArray);
+// console.log(array === newArray);
+
+function sum(...args) {
+  let ret = 0;
+  for (const v of args) {
+    ret += v;
+  }
+  return ret;
+}
+
+const result = sum(1, 2, 3, 4);
+console.log(result);
