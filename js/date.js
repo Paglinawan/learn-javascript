6047271faf1884fb6c23b5847aac5ad837ad526b
miho <paglinawan0520@gmail.com>
2024-02-27 20:13:00 +0900

    Learn: how to duplicate date

--- js/date.js
+++ js/date.js
@@ -1,9 +1,16 @@
-const myBirthDay = new Date(1994, 4, 20);
-const endOfLastMonth = new Date(2000, 3, 0);
+// const myBirthDay = new Date(1994, 4, 20);
+// const endOfLastMonth = new Date(2000, 3, 0);
 
-console.log(myBirthDay.toLocaleDateString());
-console.log(endOfLastMonth.toLocaleDateString());
+// console.log(myBirthDay.toLocaleDateString());
+// console.log(endOfLastMonth.toLocaleDateString());
 
 // myBirthDay.setDate(24);
-myBirthDay.setDate(myBirthDay.getDate() + 35);
-console.log(myBirthDay.toLocaleDateString());
+// myBirthDay.setDate(myBirthDay.getDate() + 35);
+// console.log(myBirthDay.toLocaleDateString());
+
+const original = new Date(1994, 4, 20);
+const duplicate = new Date(original.getTime());
+original.setDate(25);
+
+console.log(original.toLocaleDateString());
+console.log(duplicate.toLocaleDateString());
