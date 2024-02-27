db44a46e90e7a107511887676fece7359063d86b
miho <paglinawan0520@gmail.com>
2024-02-27 20:03:50 +0900

    Learn: setDate and getDate

--- js/date.js
+++ js/date.js
@@ -3,3 +3,7 @@ const endOfLastMonth = new Date(2000, 3, 0);
 
 console.log(myBirthDay.toLocaleDateString());
 console.log(endOfLastMonth.toLocaleDateString());
+
+// myBirthDay.setDate(24);
+myBirthDay.setDate(myBirthDay.getDate() + 35);
+console.log(myBirthDay.toLocaleDateString());
