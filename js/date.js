0d72e19992e1a27f162395003bcc234e1958196b
miho <paglinawan0520@gmail.com>
2024-02-27 20:18:25 +0900

    Learn: diff of date

--- js/date.js
+++ js/date.js
@@ -8,9 +8,13 @@
 // myBirthDay.setDate(myBirthDay.getDate() + 35);
 // console.log(myBirthDay.toLocaleDateString());
 
-const original = new Date(1994, 4, 20);
-const duplicate = new Date(original.getTime());
-original.setDate(25);
+// const original = new Date(1994, 4, 20);
+// const duplicate = new Date(original.getTime());
+// original.setDate(25);
 
-console.log(original.toLocaleDateString());
-console.log(duplicate.toLocaleDateString());
+// console.log(original.toLocaleDateString());
+// console.log(duplicate.toLocaleDateString());
+
+const diff = Date.now() - new Date(2024, 1, 0).getTime();
+const days = Math.floor(diff / 1000 / 60 / 60 / 24);
+console.log(days + "日前");
