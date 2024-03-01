e8baab00093ee3ac85052cafc07ea2b96083fc6c
miho <paglinawan0520@gmail.com>
2024-03-01 18:19:39 +0900

    Learn: recursive function

--- js/setTimeout.js
+++ js/setTimeout.js
@@ -1,7 +1,20 @@
 // 指定した時間後に一度だけ処理を実行する
 
-setTimeout(() => {
-  console.log("How are you?");
-}, 3000);
+// setTimeout(() => {
+//   console.log("How are you?");
+// }, 3000);
 
-console.log("Hello!");
+// console.log("Hello!");
+
+// ブラウザに負荷をかけずに繰り返し実行できる
+let count = 3;
+function showClock() {
+  const timeoutId = setTimeout(() => {
+    console.log(new Date());
+    count--;
+    if (count < 0) return;
+    // 再帰関数
+    showClock();
+  }, 1000);
+}
+showClock();
