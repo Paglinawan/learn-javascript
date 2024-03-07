3678f048ea703923964071206290d70f78e571e7
miho <paglinawan0520@gmail.com>
2024-03-07 21:52:54 +0900

    Learn: req.query

--- node/app_express.mjs
+++ node/app_express.mjs
@@ -6,8 +6,15 @@ const app = express();
 app.get("/", function (req, res) {
   res.send(`
     <p>Hello Miho. How are you?</p>
+    <p>You wanna see response?? Then <a href="result/?param1=1&param2=2">Click Here!</a></p>
   `);
 });
+
+app.get("/result", function (req, res) {
+  const params = req.query;
+  console.log(params);
+});
+
 app.listen(PORT, () => {
   console.log("Hello!");
 });
