4580781ca630c571077cb8518b7d03431a2a0d10
miho <paglinawan0520@gmail.com>
2024-03-09 11:03:11 +0900

    Learn: express and json , post

--- node/app_json.mjs
+++ node/app_json.mjs
@@ -3,15 +3,39 @@ import express from "express";
 const PORT = 8082;
 const app = express();
 
+app.use(express.json());
+
 app.get("/", function (req, res) {
-  res.send({
-    message: "How are you?",
-    number: 1,
-    array: ["pink", "yellow", "tomato"],
-  });
-  // send: 適宜調整
-  // json: jsonデータに限定
-  // end: 文字列のみ
+  res.send(`
+    <form action="/form_result" method="POST">
+      <input type="text" name="sample"></input>
+      <button type="submit">SUBMIT</button>
+    </form>
+    <script>
+      const formEl = document.querySelector('form');
+      formEl.onsubmit = function(e) {
+        e.preventDefault();
+        const sample = formEl[0].value;
+
+        const data = {
+          sample
+        };
+
+        fetch("/form_result", {
+          method: "post",
+          headers: {
+            "content-type": "application/json"
+          },
+          body: JSON.stringify(data)
+        })
+      }
+    </script>
+  `);
+});
+
+app.post("/form_result", function (req, res) {
+  const params = req.body;
+  console.log(params);
 });
 
 app.listen(PORT, () => {
