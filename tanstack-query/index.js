20042130dfe5f32f50c89e3a3342d483d75f4f53
miho <paglinawan0520@gmail.com>
2024-03-18 22:32:37 +0900

    Learn: adjust Prisma

--- tanstack-query/index.js
+++ tanstack-query/index.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const { PrismaClient } = require("@prisma/client");
 
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.send("Hello World!"));
+const prisma = new PrismaClient();
+app.get("/todos", async (req, res) => {
+  const todos = await prisma.todo.findMany();
+  return res.json(todos);
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
