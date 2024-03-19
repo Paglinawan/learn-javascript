78b86a169b22505217a48f0c619d2b77b93496e6
miho <paglinawan0520@gmail.com>
2024-03-19 20:44:29 +0900

    Learn: TanStack Query (旧React Query)

--- tanstack/src/Todo.jsx
+++ tanstack/src/Todo.jsx
@@ -1,7 +1,24 @@
+import { useQuery } from "@tanstack/react-query";
+
+const fetchTodos = async () => {
+  const res = await fetch("http://localhost:5173/todos");
+  return res.json();
+};
+
 const Todo = () => {
+  const { data: todos } = useQuery({
+    queryKey: ["todos"],
+    queryFn: fetchTodos,
+  });
+
   return (
     <>
-      <h1>ToDo List</h1>
+      <h1>Todo一覧</h1>
+      <ul>
+        {todos?.map((todo) => (
+          <li key={todo.id}>{todo.name}</li>
+        ))}
+      </ul>
     </>
   );
 };
